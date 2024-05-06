06a51c2ae84237826a0baf9eeba47b2cee1a4d62
veronicasteen <veronica.steen@outlook.com>
2024-05-06 11:34:05 +0200

    Updated filter- and search-function
    - Category is not done yet.

--- VOSA-Events/Pages/Index.cshtml.cs
+++ VOSA-Events/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using VOSA_Events.Data;
 using VOSA_Events.Models;
 
@@ -19,9 +20,46 @@ namespace VOSA_Events.Pages
             this.database = database;
         }
 
-        public void OnGet()
+        public void OnGet(int pageNumber, string searchItem, string category, string date)
         {
+			PageNumber = pageNumber;
+			var pageSize = 10;
 
-        }
-    }
+			Events = database.Events.ToList();
+
+			if (!string.IsNullOrEmpty(searchItem))
+			{
+				Events = Events.Where(p => p.Name.ToLower().Contains(searchItem)).ToList();
+			}
+
+			//if (!string.IsNullOrEmpty(category) && category != "Any category")
+			//{
+			//	Category = category.ToLower();
+			//}
+
+			if (date == "Closest in time")
+			{
+				Events = Events.OrderByDescending(e => e.Date).ToList();
+
+			}
+
+			if (date == "Farthest in time")
+			{
+				Events = Events.OrderBy(e => e.Date).ToList();
+
+			}
+
+			int totalEvents = Events.Count;
+			TotalPages = (int)Math.Ceiling((double)totalEvents / pageSize);
+
+			if (PageNumber < 1)
+			{
+				PageNumber = 1;
+			}
+
+			Events = Events.Skip((pageNumber - 1) * pageSize)
+				.Take(pageSize).ToList();
+
+		}
+	}
 }
