421c4839ce62ec8a941c38fadbf0b3d35f9e7943
oalmhage <142837730+oalmhage@users.noreply.github.com>
2024-05-06 11:53:59 +0200

    EventDetails
    Added details in eventdetails. Also added booking logic

--- VOSA-Events/Pages/EventDetails.cshtml.cs
+++ VOSA-Events/Pages/EventDetails.cshtml.cs
@@ -1,12 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VOSA_Events.Data;
+using VOSA_Events.Models;
 
 namespace VOSA_Events.Pages
 {
-    public class EventDetailsModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class EventDetailsModel : PageModel
+	{
+
+		private readonly AppDbContext database;
+		private readonly AccessControl accessControl;
+
+
+		public EventDetailsModel(AppDbContext database, AccessControl accessControl)
+		{
+			this.database = database;
+			this.accessControl = accessControl;
+		}
+
+		public Event Event { get; set; }
+
+		public void OnGet(int id)
+		{
+			Event = database.Events.Find(id);
+		}
+		public IActionResult OnPost(int id)
+		{
+
+			var loggedInUserId = accessControl.LoggedInAccountID;
+
+			var existingEvent = database.Bookings.SingleOrDefault(ap => ap.AccountID == loggedInUserId && ap.EventID == id);
+
+			if (existingEvent != null)
+			{
+				existingEvent.Quantity++;
+			}
+
+			else
+			{
+				var bookings = new Booking
+				{
+					EventID = id,
+					AccountID = loggedInUserId,
+					Quantity = 1
+				};
+
+				database.Bookings.Add(bookings);
+			}
+
+			database.SaveChanges();
+
+			return RedirectToPage("/Index");
+		}
+	}
 }
