b77feec10bf05d2d748655f54b4d87ee3e87ecaf
veronicasteen <veronica.steen@outlook.com>
2024-05-17 11:57:31 +0200

    Changed int to double in Event.cs

--- VOSA-Events/Models/Event.cs
+++ VOSA-Events/Models/Event.cs
@@ -4,7 +4,7 @@
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public string Description { get; set; }
         public string City { get; set; }
         public DateTime Date { get; set; }
@@ -22,7 +22,7 @@
     public class EventDto
     {
         public string Name { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public string Description { get; set; }
         public string City { get; set; }
         public DateTime Date { get; set; }
